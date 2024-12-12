aa8935c82d4db50eb13bccc286622b5d5f1b6dc5
Hammad <165670673+Hammadabbasi17@users.noreply.github.com>
2024-12-11 22:45:17 -0800

    Update page.tsx

--- src/app/Pages/page.tsx
+++ src/app/Pages/page.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <>
                <div className="mt-[84px]>
                   <p>This is a pages</p> 
                </div>
-    </div>
+    </>
   )
 }
 
